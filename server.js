c0db99c23e815a66f9087324049c66616c7b6c6a
aydinjaan-lab <aydinjaan@gmail.com>
2026-01-18 19:21:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.get("/ask", async (req, res) => {
           messages: [
             {
               role: "system",
-              content: "Reply properly in simple English. No symbols or emojis."
+              content: "Reply properly in simple English. No symbols or emojis just text emoticons if possible."
             },
             {
               role: "user",
